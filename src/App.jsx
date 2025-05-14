b570374e0c764e1c37ad4d53e357c2b403695ac4
Jason <394404228@qq.com>
2025-05-14 20:12:08 +1000

    Revert "upload client settings page"
    This reverts commit 62d67c01ad5c14df1ec51eb037416829d6609a63.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
 import AdminLoginPage from "./pages/AdminLoginPage";
 import AdminDashboardPage from "./pages/AdminDashboardPage";
 import AdminDetectionPage from "./pages/AdminDetectionPage";
-import AdminPredictionPage from "./pages/AdminPredictionPage";
-import AdminReportsPage from "./pages/AdminReportsPage";
+import UploadDatasetPage from "./pages/UploadDatasetPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ClientDashboardPage from "./pages/ClientDashboardPage";
+import QuickScanPage from "./pages/QuickScanPage";
+import ClientHistoryPage from "./pages/ClientHistoryPage";
 import AdminScanResultPage from "./pages/AdminScanResultPage";
+import AdminReportsPage from "./pages/AdminReportsPage";
+import AdminPredictionPage from "./pages/AdminPredictionPage";
+import ClientScanResultPage from "./pages/ClientScanResultPage";
+import ClientReportPage from "./pages/ClientReportPage";
 import AdminSettingsPage from "./pages/AdminSettingsPage";
 import AdminNotificationsPage from "./pages/AdminNotificationsPage";
-import ClientDashboardPage from "./pages/ClientDashboardPage";
-import ClientHistoryPage from "./pages/ClientHistoryPage";
-import ClientReportPage from "./pages/ClientReportPage";
-import ClientScanResultPage from "./pages/ClientScanResultPage";
-import QuickScanPage from "./pages/QuickScanPage";
-import UploadDatasetPage from "./pages/UploadDatasetPage";
-import ClientSettingsPage from "./pages/ClientSettingsPage";
 
 function App() {
   return (
@@ -24,24 +23,20 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/admin" element={<AdminLoginPage />} />
-        <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
-        <Route path="/admin/detection" element={<AdminDetectionPage />} />
-        <Route path="/admin/prediction" element={<AdminPredictionPage />} />
-        <Route path="/admin/reports" element={<AdminReportsPage />} />
-        <Route path="/admin/scan-result" element={<AdminScanResultPage />} />
-        <Route path="/admin/settings" element={<AdminSettingsPage />} />
-        <Route
-          path="/admin/notifications"
-          element={<AdminNotificationsPage />}
-        />
-        <Route path="/client/dashboard" element={<ClientDashboardPage />} />
-        <Route path="/client/history" element={<ClientHistoryPage />} />
-        <Route path="/client/report" element={<ClientReportPage />} />
-        <Route path="/client/scan-result" element={<ClientScanResultPage />} />
-        <Route path="/client/quick-scan" element={<QuickScanPage />} />
-        <Route path="/client/upload-dataset" element={<UploadDatasetPage />} />
-        <Route path="/client/settings" element={<ClientSettingsPage />} />
+        <Route path="/admin-login" element={<AdminLoginPage />} />
+        <Route path="/admin-dashboard" element={<AdminDashboardPage />} />
+        <Route path="/admin-detection" element={<AdminDetectionPage />} />
+        <Route path="/upload-dataset" element={<UploadDatasetPage />} />
+        <Route path="/client-dashboard" element={<ClientDashboardPage />} />
+        <Route path="/client-history" element={<ClientHistoryPage />} />
+        <Route path="/quick-scan" element={<QuickScanPage />} />
+        <Route path="/admin-scan-results" element={<AdminScanResultPage />} />
+        <Route path="/admin-reports" element={<AdminReportsPage />} />
+        <Route path="/admin-prediction" element={<AdminPredictionPage />} />
+        <Route path="/client-scan-results" element={<ClientScanResultPage />} />
+        <Route path="/client-report" element={<ClientReportPage />} />
+        <Route path="/admin-settings" element={<AdminSettingsPage />} />
+        <Route path="/admin-notifications" element={<AdminNotificationsPage />} />
       </Routes>
     </Router>
   );
