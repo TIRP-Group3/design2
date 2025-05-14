da3fb55a6091fc5995eb61891ffda5c74b077b48
Jason <394404228@qq.com>
2025-05-14 20:11:56 +1000

    Revert "upload"
    This reverts commit 832e1587628fd4bd9af0c2e3121df566f77903fc.

--- src/pages/ClientNotificationsPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ClientNotifications from "../components/ClientNotifications";
-
-function ClientNotificationsPage() {
-  return <ClientNotifications />;
-}
-
-export default ClientNotificationsPage;
